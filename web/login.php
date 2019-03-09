585fbb5d66397a42b8ff1fb9334a4a5423daac78
Freshtaco <lang372@gmail.com>
2019-03-09 14:11:18 +0100

    added basic authentication

--- web/login.php
+++ web/login.php
@@ -4,11 +4,9 @@ $pwd = "1234";
 
 if(isset($_GET['password'])) {
 
-     if($_GET['password'] == $pwd) {
-          // setcookie('uid', $new_uid, time() + 60*60*24*30, '/');
-          // header("Location: $url");
-          // exit;
-          
+     if($_GET['password'] != $pwd) {
+          echo "invalid password";
+          exit;
      }
 
 }
