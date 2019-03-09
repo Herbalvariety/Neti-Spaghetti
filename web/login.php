a186a89f474cf67c34ae7742a2a2298d1699669e
Freshtaco <lang372@gmail.com>
2019-03-09 09:13:43 +0100

    new files

---
+++ web/login.php
@@ -0,0 +1,16 @@
+<?php
+
+$pwd = "1234";
+
+if(isset($_GET['password'])) {
+
+     if($_GET['password'] == $pwd) {
+          // setcookie('uid', $new_uid, time() + 60*60*24*30, '/');
+          // header("Location: $url");
+          // exit;
+          
+     }
+
+}
+
+?>
